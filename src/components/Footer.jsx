4c1080b345b641cc225e8738f5f7436fec44304e
saisushmayadla <saisushmayadla@gmail.com>
2026-07-11 22:54:00 +0530

    Improvements

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+function Footer() {
+  return (
+    <footer
+      style={{
+        textAlign: "center",
+        padding: "30px",
+        marginTop: "60px",
+        color: "#6B7280",
+      }}
+    >
+      © 2026 StudySphere • Built for MDC Round 2
+    </footer>
+  );
+}
+
+export default Footer;
